3b64d488d73877a767bbfaf2df0efc4fa721dfa3
a419797296 <deep-diary@qq.com>
2023-06-29 08:14:11 +0800

    change the config file from py to json. change the .gitignore to exclude the config file

--- Backend/frontend/dist/static/js/chunk-2d0ac96a.5dd08242.js
+++
@@ -1,7 +0,0 @@
-/*!
- *  build: vue-admin-better 
- *  vue-admin-beautiful.com 
- *  https://gitee.com/chu1204505056/vue-admin-better 
- *  time: 2022-8-30 07:03:59
- */
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ac96a"],{"19d0":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("这里是事物页的vue。。。")])])}],a={name:"PgObject",components:{}},r=a,l=t("2877"),s=Object(l["a"])(r,c,u,!1,null,null,null);n["default"]=s.exports}}]);
--- Backend/frontend/dist/static/js/chunk-2d0b3287.db01c7f5.js
+++
@@ -1,7 +0,0 @@
-/*!
- *  build: vue-admin-better 
- *  vue-admin-beautiful.com 
- *  https://gitee.com/chu1204505056/vue-admin-better 
- *  time: 2022-8-30 07:03:59
- */
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b3287"],{"26d1":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("这里是地点页的vue。。。")])])}],s={name:"PgAddress",components:{}},u=s,l=t("2877"),a=Object(l["a"])(u,c,r,!1,null,null,null);n["default"]=a.exports}}]);
