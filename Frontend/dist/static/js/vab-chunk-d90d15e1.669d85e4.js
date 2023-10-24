60fdd3f3580bffa90105487e3d10b75e883094c4
a419797296 <deep-diary@qq.com>
2023-10-24 19:43:40 +0800

    add the build version of frontend

--- Frontend/dist/static/js/vab-chunk-d90d15e1.669d85e4.js
+++ Frontend/dist/static/js/vab-chunk-d90d15e1.669d85e4.js
@@ -2,7 +2,7 @@
  *  build: vue-admin-better 
  *  vue-admin-beautiful.com 
  *  https://gitee.com/chu1204505056/vue-admin-better 
- *  time: 2023-10-6 15:18:13
+ *  time: 2023-10-24 19:41:02
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["vab-chunk-d90d15e1"],{1157:function(e,t,n){var r,i;
 /*!
